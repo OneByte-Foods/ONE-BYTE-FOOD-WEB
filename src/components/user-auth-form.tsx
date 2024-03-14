e91563d1d93b4f9b447d29433d2922904e462432
Bikalpa_ <bikalpapaudel53@gmail.com>
2024-03-14 22:04:10 +0545

    feat:/ signup ui

---
+++ src/components/user-auth-form.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+
+export function UserAuthForm() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className="grid gap-4">
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-3">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+            <Label className="sr-only" htmlFor="password">
+              password
+            </Label>
+            <Input
+              id="cpassword"
+              placeholder="password"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="on"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+            <Label className="sr-only" htmlFor="cpassword">
+              confirm password
+            </Label>
+            <Input
+              id="cpassword"
+              placeholder="confirm password"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="on"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>Sign Up with Email</Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button
+        variant="outline"
+        type="button"
+        disabled={isLoading}
+        className="flex items-center gap-2 text-xl"
+      >
+        <FcGoogle /> <span>Google</span>
+      </Button>
+    </div>
+  );
+}
