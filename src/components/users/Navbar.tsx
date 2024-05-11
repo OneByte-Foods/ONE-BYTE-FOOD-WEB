e3f39568eaf7a26543d1a56cc1d72af03d9e98bb
Bikalpa_ <bikalpapaudel53@gmail.com>
2024-05-11 19:41:31 +0545

    register restaurant

--- src/components/users/Navbar.tsx
+++ src/components/users/Navbar.tsx
@@ -8,30 +8,51 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { logout } from "@/firebase/config";
+import { db, logout } from "@/firebase/config";
+import { doc, getDoc } from "firebase/firestore";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { MdLocationPin } from "react-icons/md";
 import { useSelector } from "react-redux";
+import { setUser } from "../../../redux/features/users-slice";
 import { RootState } from "redux/reducer";
+import { useDispatch } from "react-redux";
 
 function Navbar() {
   // const [user, setUser] = useState<any>(null);
   const { uid } = useSelector((state: RootState) => state.auth);
+  const { username, roles, imageUrl } = useSelector(
+    (state: RootState) => state.users
+  );
+  const dispatch = useDispatch();
 
   async function handleLogout() {
     logout();
-    // localStorage.removeItem("user")
+    localStorage.removeItem("uid");
   }
 
   useEffect(() => {
-    // setUser(
-    //   localStorage.getItem("user")
-    //     ? JSON.parse(localStorage.getItem("user") || "{}")
-    //     : null
-    // );
-  }, []);
+    const fetchUserData = async () => {
+      if (uid) {
+        try {
+          const docRef = doc(db, "users", uid);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+            const userData = docSnap.data();
+            console.log("Document data:", userData);
+            dispatch(setUser(userData)); // Assuming you have a setUser action in your auth slice
+          } else {
+            console.log("No such document!");
+          }
+        } catch (error) {
+          console.error("Error getting document:", error);
+        }
+      }
+    };
+
+    fetchUserData();
+  }, [uid, dispatch]);
 
   return (
     <header className="flex h-[10vh] px-[5%] md:px-[10%] items-center justify-between">
@@ -40,32 +61,33 @@ function Navbar() {
           One Byte <span className="text-[#FFB30E]">Food</span>
         </h1>
         <span className="text-[10px] flex items-center gap-2">
-          <MdLocationPin />
-          Naxal, 01, Kathmandu
+          managaing restaurnat
+         
         </span>
       </div>
       <nav className="flex flex-col gap-1">
         <ul className="flex gap-4 font-bold items-center">
           <li>
-            <Link href="/menu">Menu</Link>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/users">Users</a>
+            {roles.includes("admin") ? (
+              <Link href="/dashboard">Manage Restaurant</Link>
+            ) : (
+              <Link href="/register">Register Restaurant</Link>
+            )}
           </li>
         </ul>
       </nav>
-      {true ? (
+      {username ? (
         <DropdownMenu>
           <DropdownMenuTrigger className=" h-10 w-10 border rounded-full overflow-hidden">
-            {/* {user && <img src={user.photoURL} />} */}
+            <img src={imageUrl} />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/restaurants">Restaurants</Link>
+            </DropdownMenuItem>
             <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
